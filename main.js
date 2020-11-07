1b4d5193f309d0cab1668ae3012a963ee9b7b750
sergiipoliakov <64687462+sergiipoliakov@users.noreply.github.com>
2020-11-07 12:51:37 +0200

    add1

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 const colors = [
-	"#FFFFFF",
-	"#2196F3",
-	"#4CAF50",
-	"#FF9800",
-	"#009688",
-	"#795548",
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
 ];
 
+const bodyEl = document.querySelector('body');
+const startBtn = document.querySelector(
+  'button[data-action="start"]',
+);
+const stopBtn = document.querySelector('button[data-action="stop"]');
+
 const randomIntegerFromInterval = (min, max) => {
-	return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  const intervalId = setInterval(() => {
+    const randonColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+
+    bodyEl.style.backgroundColor = randonColor;
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+}
